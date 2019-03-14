ecf2c2aa41c56f8c1a7edd72448faf972e7e45e5
RelatedKibbles <d.brick461@gmail.com>
2019-03-13 20:27:08 -0500

    Create ActualWriterTM.py

---
+++ ActualWriterTM.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 20:19:34 2019
+
+@author: Declan
+"""
+
+def ActualWriterTM(Date,Objects,Frequencies,Times,Filenames):
+    '''
+    Takes the date formatting, the dictionary of objects, the dictionary of frequencies,
+    the dictionary of times, and the dictionary of filenames
+    formatting is assumed to be like:
+    writes the .cmd file in the proper formatting 
+    '''
+    
+    return 
