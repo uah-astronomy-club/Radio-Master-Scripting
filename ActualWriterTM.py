2ba208c7dc33874b60be9c5aa4364116f876b7f5
RelatedKibbles <d.brick461@gmail.com>
2019-03-27 20:40:17 -0500

    Update ActualWriterTM.py

--- ActualWriterTM.py
+++ ActualWriterTM.py
@@ -12,12 +12,38 @@ def ActualWriterTM(Date,Objects,Frequencies,Times,Filenames):
     formatting is assumed to be like:
     writes the .cmd file in the proper formatting 
     '''
+    freq=0
+    mode=0
+    with open(Filenames['directoryname']+Filenames['filename']+'.txt','w+') as cmdfile:   
+        for Object in Objects.keys():
+            f=Frequencies.keys()
+            if 'all' in f:
+                s=Frequencies['all']
+                s=s.split()
+                i=s.index(',')
+                for c in range(i):
+                    freq+=c
+                for c in range(i,len(s)):
+                    mode+=c
+            else:
+                s=Frequencies[Object]
+                s=s.split()
+                i=s.index(',')
+                for c in range(i):
+                    freq+=c
+                for c in range(i,len(s)):
+                    mode+=c
+            cmdfile.write(': freq'+freq+' '+mode+'\n')
+            if Objects[Object]=='named':
+                cmdfile.write(': '+Object+'\n')
+            elif Objects[Object]=='Galactic':
+                cmdfile.write(': galactic'+Object+'\n')
     #for object
-    #calibrate telescope
     #set freq and mode 
     #point at the object
     #start datafile
     #insert time for integration
     #stop data file
+    #calibrate telescope if needed
     #next object 
     return 
