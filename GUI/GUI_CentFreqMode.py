6c6c908cfc011413b686d50458498609039f8640
Brandon Staton <48103710+Jinxingdaing@users.noreply.github.com>
2020-10-14 22:02:08 -0400

    update

--- GUI/GUI_CentFreqMode.py
+++ GUI/GUI_CentFreqMode.py
@@ -4,75 +4,19 @@ Created on Wed Feb 13 19:50:37 2019
 
 @author: Declan
 """
-def main(objectlist):
+def main(i,freq,mode):
     '''
     Input: dictionary of objects w/ obj name as the key
     Return: dict of {object name:'frequency,mode'}
     '''
     frequencydict={}
     
+    # makes all keys 2 characters
+    if int(i)< 10:
+        i = '0' + str(int(i))
+        
+    freq = str(freq)
+    mode = str(mode)
     
-    
-    return
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Orignal code
-#==============================================================================
-# def frequencysetter(objectlist):
-#     '''
-#     takes a dictionary of objects with the name of the object as the key
-#     returns a dictionary of {object name:'frequency,mode'}, note that the dictionary is in reverse order from the input dictionary
-#     if one frequency the key is: 'all'
-#     '''
-#     frequencydict={}
-#     while True:    
-#         setall=input('Would you like to enter the same frequency settings for all objects you wish to observe?\nEnter yes or no. ')
-#         if setall.lower()=='yes':
-#             while True:
-#                 freq=input('Enter the centring frequency in MHz from 1301 to 1799 MHz: ')
-#                 if not (1300<float(freq)<1800):     # Changed to include 1300 and 1800 MHz
-#                     print("That's outside the telescope's measuring range of 1300-1800 MHz" )
-#                 else:
-#                     break
-#             while True:   
-#                 mode=input('Enter the mode (1,2,3,4) you want to observe in: ')
-#                 List=[1,2,3,4]
-#                 if int(mode) not in List:
-#                     print('That is not a valid observing mode! Maybe you should stick to binoculars...')
-#                 else:
-#                     frequencydict.update({'all':freq+','+mode})
-#                     break
-#             break
-#         elif setall.lower()=='no':
-#             for thing in objectlist.keys():
-#                 print("Current object:"+thing)#note that this expects the key to be a string
-#                 while True:
-#                     currentfreq=input('Enter the centring frequency for the current object from 1300 to 1800 MHz: ')
-#                     if not (1300<=float(currentfreq)<=1800):
-#                         print("That's outside the telescope's measuring range of 1300-1800 MHz.")
-#                     else:
-#                         break
-#                 while True:
-#                     currentmode=input('Enter the mode (1,2,3,4) you want to observe in: ')
-#                     List=[1,2,3,4]
-#                     if int(currentmode) not in List:
-#                         print('That is not a valid observing mode! Maybe you should stick to binoculars...')
-#                     else:
-#                         frequencydict.update({thing:currentfreq+','+currentmode})#note that this epects the key to be a string
-#                         break
-#             break
-#         else:
-#             print("You didn't enter yes or no, this is not a hard question-what are you? a biology major?")
-#     return frequencydict
+    frequencydict.update({i:freq+','+mode})
+    return frequencydict
--- GUI/GUI_Master_Writer.py
+++ GUI/GUI_Master_Writer.py
@@ -8,6 +8,7 @@ Created on Fri Jun 12 14:49:38 2020
 import GUI_TellieTime as TT
 import GUI_TelescopePointer as TP
 import GUI_CentFreqMode as CFM
+import GUI_TimeAndDataPoints as TDP
 
 def main(objects):
     files_info = objects[0].split()
@@ -30,6 +31,11 @@ def main(objects):
                                              current_object_info[3])
         
         # Sends the info to center frequ
+        freq_info = CFM.main(i,current_object_info[4], current_object_info[5])
+        
+        #Sends info to time and data points
+        
+        
         
         
         
@@ -52,6 +58,10 @@ def main(objects):
         print('Object Dict: ' + str(object_dictionary))
     return
 
-main(['C:\\Users\\bstat\\Documents\\GitHub\\Radio-Master-Scripting\\GUI cmd rad', '2020-10-15 00:00:00 Listed 70G 1301 1 1 1 rad'])
+#['2020-10-15 00:00:00 Listed 40G 1301 1 1 1 rad', '2020-10-16 00:00:00 Listed 60G 1304 2 2 2 rad2']
+#['C:\\Users\\bstat\\Documents\\GitHub\\Radio-Master-Scripting\\GUI cmd rad2', '2020-10-15 00:00:00 Listed 40G 1301 1 1 1 rad', '2020-10-16 00:00:00 Listed 60G 1304 2 2 2 rad2']
+
+
+main(['C:\\Users\\bstat\\Documents\\GitHub\\Radio-Master-Scripting\\GUI cmd rad2', '2020-10-15 00:00:00 Listed 40G 1301 1 1 1 rad', '2020-10-16 00:00:00 Listed 60G 1304 2 2 2 rad2'])
 
 # add integration time to observation start time to determine observation end time
